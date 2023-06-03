c484ba334c1e3d6e7f2ce16e6aa8c949ebf3e70c
home <home@MacBook-Pro-home.local>
2023-06-03 18:27:56 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [file, setFile] = useState();
+  const [download, setDownload] = useState();
+
+  const handleInputChange = (e) => {
+    setFile(e.target.files[0]);
+    console.log(e.target.files[0].name);
+    setDownload(URL.createObjectURL(e.target.files[0]));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleInputChange}></input>
+      <a href={download} > Download: {file?.name}</a>
     </div>
   );
 }
